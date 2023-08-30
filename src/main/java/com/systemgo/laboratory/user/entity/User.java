ce50f6fbb32cafa9ac4a15307df3cf7119c422b5
Leandro Oliveira Camargo <leandrocamargo@sisconsultoria.com.br>
2023-08-30 15:35:08 -0300

    H2 database, test profile, JPA

--- src/main/java/com/systemgo/laboratory/user/entity/User.java
+++ src/main/java/com/systemgo/laboratory/user/entity/User.java
@@ -1,9 +1,14 @@
 package com.systemgo.laboratory.user.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.UUID;
 
 @Entity
+@Table(name = "tb_user")
 public class User {
     @Id
     @GeneratedValue
