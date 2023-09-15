245e24a31fbe41af21d1fa93813fbdd749f2640d
Leandro Oliveira Camargo <leandrocamargo@sisconsultoria.com.br>
2023-09-15 10:43:39 -0300

    exception find by id

--- src/main/java/com/systemgo/laboratory/user/UserService.java
+++ src/main/java/com/systemgo/laboratory/user/UserService.java
@@ -1,5 +1,6 @@
 package com.systemgo.laboratory.user;
 
+import com.systemgo.laboratory.exceptions.ResourceNotFoundException;
 import com.systemgo.laboratory.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(UUID id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
